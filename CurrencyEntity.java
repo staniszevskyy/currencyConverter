00dc5756c88b8ddfb41f61dc763808afc7da93c8
staniszevskyy <expunge96@hotmail.com>
2018-10-14 16:35:52 +0200

    added class representing currency entity

---
+++ CurrencyEntity.java
@@ -0,0 +1,30 @@
+public class CurrencyEntity {
+    private String code;
+    private String name;
+    private double multipliciand;
+    private double exchangeRate;
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMultipliciand() {
+        return multipliciand;
+    }
+
+    public double getExchangeRate() {
+        return exchangeRate;
+    }
+
+    public void setMultipliciand(double multipliciand) {
+        this.multipliciand = multipliciand;
+    }
+
+    public void setExchangeRate(double exchangeRate) {
+        this.exchangeRate = exchangeRate;
+    }
+}
